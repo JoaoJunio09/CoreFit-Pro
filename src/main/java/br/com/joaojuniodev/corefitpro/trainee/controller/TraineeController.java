c14b91e99950337ad61682c271aff240dfef99ec
JoaoJunio09 <joaojunio818@gmail.com>
2026-09-08 17:02:52 -0300

    feat: service e controller de Trainee

--- src/main/java/br/com/joaojuniodev/corefitpro/trainee/controller/TraineeController.java
+++ src/main/java/br/com/joaojuniodev/corefitpro/trainee/controller/TraineeController.java
@@ -1,4 +1,47 @@
 package br.com.joaojuniodev.corefitpro.trainee.controller;
 
+import br.com.joaojuniodev.corefitpro.trainee.dto.request.TraineeRequestDTO;
+import br.com.joaojuniodev.corefitpro.trainee.dto.response.TraineeResponseDTO;
+import br.com.joaojuniodev.corefitpro.trainee.service.TraineeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/trainees/v1")
 public class TraineeController {
+
+    private final TraineeService traineeService;
+
+    public TraineeController(TraineeService traineeService) {
+        this.traineeService = traineeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TraineeResponseDTO>> getAll() {
+        return ResponseEntity.ok().body(traineeService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TraineeResponseDTO> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok().body(traineeService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TraineeResponseDTO> create(@RequestBody TraineeRequestDTO trainee) {
+        return ResponseEntity.ok().body(traineeService.create(trainee));
+    }
+
+    @PutMapping
+    public ResponseEntity<TraineeResponseDTO> update(@RequestBody TraineeRequestDTO trainee) {
+        return ResponseEntity.ok().body(traineeService.update(trainee));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+        traineeService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/com/joaojuniodev/corefitpro/trainee/service/TraineeService.java
+++ src/main/java/br/com/joaojuniodev/corefitpro/trainee/service/TraineeService.java
@@ -1,4 +1,70 @@
 package br.com.joaojuniodev.corefitpro.trainee.service;
 
+import br.com.joaojuniodev.corefitpro.exceptions.NotFoundException;
+import br.com.joaojuniodev.corefitpro.mapper.trainee.TraineeMapper;
+import br.com.joaojuniodev.corefitpro.trainee.dto.request.TraineeRequestDTO;
+import br.com.joaojuniodev.corefitpro.trainee.dto.response.TraineeResponseDTO;
+import br.com.joaojuniodev.corefitpro.trainee.repository.TraineeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class TraineeService {
+    private static final Logger logger = LoggerFactory.getLogger(TraineeService.class.getName());
+
+    private final TraineeRepository traineeRepository;
+    private final TraineeMapper traineeMapper;
+
+    public TraineeService(TraineeRepository traineeRepository, TraineeMapper traineeMapper) {
+        this.traineeRepository = traineeRepository;
+        this.traineeMapper = traineeMapper;
+    }
+
+    public List<TraineeResponseDTO> getAll() {
+        logger.info("Getting All Trainees");
+
+        return traineeRepository.findAll()
+            .stream()
+            .map(traineeMapper::toResponse)
+            .toList();
+    }
+
+    public TraineeResponseDTO getById(UUID id) {
+        logger.info("Getting By Trainee Id");
+
+        var entity = traineeRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Not found this Trainee Id: " + id));
+        return traineeMapper.toResponse(entity);
+    }
+
+    public TraineeResponseDTO create(TraineeRequestDTO trainee) {
+        logger.info("Creating new Exercise");
+
+        var entity = traineeMapper.toEntity(trainee);
+        var traineeCreated = traineeRepository.save(entity);
+        return traineeMapper.toResponse(traineeCreated);
+    }
+
+    public TraineeResponseDTO update(TraineeRequestDTO trainee) {
+        logger.info("Updating Exercise");
+
+        var entity = traineeRepository.findById(trainee.id())
+            .orElseThrow(() -> new NotFoundException("Not found this Trainee Id: " + trainee.id()));
+        entity.setFirstName(trainee.firstName());
+        entity.setLastName(trainee.lastName());
+        var traineeUpdated = traineeRepository.save(entity);
+        return traineeMapper.toResponse(traineeUpdated);
+    }
+
+    public void delete(UUID id) {
+        logger.info("Deleting Trainee");
+
+        var entity = traineeRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Not found this Trainee Id: " + id));
+        traineeRepository.delete(entity);
+    }
 }
